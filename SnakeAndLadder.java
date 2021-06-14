240a0a4dc4b234c0f48e3541744aaf22125a79ff
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-14 18:46:29 +0530

    Added use case 2

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,21 +1,22 @@
 /**
- * Snake and Ladder Program.
- * Snake and Ladder game played with single player at start position 0
+ * Snake and ladder program
+ * The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the number between 1 to 6
  *
  * @author Pratik Chaudhari
  * @since 14/06/2021
  */
 package day4;
 
-public class SnakeAndLadderUC1 {
-	int position = 0;
+public class SnakeAndLadderUC2 {
+	int position = 0; int dieRoll;
 	void PlayerPosition(int position) {
 		System.out.println("Player position is "+position);
+		dieRoll = (int)((Math.floor(Math.random() * 10)) % 6) + 1;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game simulation.");
-		SnakeAndLadderUC1 Player1 = new SnakeAndLadderUC1();
+		SnakeAndLadderUC2 Player1 = new SnakeAndLadderUC2();
 		Player1.PlayerPosition(0);
 	}
 
