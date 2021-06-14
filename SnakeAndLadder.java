b2632b1ae32aa5a24cdf02aab0e3c89c5a91572e
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-14 19:00:43 +0530

    Added Use case 5

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,13 +1,16 @@
 /**
- * Repeat till the Player reaches the winning position 100.
- * - Note In case the player position moves below 0, then the player restarts from 0
- *
+ * Ensure the player gets to exact winning position 100.
+ * - Note in case the player position go above 100,
+ * the player stays in the same previous position,
+ * till the player gets the exact number that adds upto 100.
+ * 
  * @author Pratik Chaudhari
  * @since 14/06/2021
  */
+
 package day4;
 
-public class SnakeAndLadderUC4 {
+public class SnakeAndLadderUC5 {
 
 	int initialPosition = 0, position = 0, WinPosition = 100, dieRoll, choice;
 	final int noPlay = 0;
@@ -21,18 +24,21 @@ public class SnakeAndLadderUC4 {
 			dieRoll = (int) ((Math.floor(Math.random() * 10)) % 6) + 1;
 			choice = (int) ((Math.floor(Math.random() * 10)) % 3);
 
-			if (choice == noPlay) {
+			if (choice == noPlay) {			// no move.
 				System.out.println("Player does not move.");
 
 			} else if (choice == ladder) {
 				position += dieRoll;
-				if (position == WinPosition) {
+				if (position > WinPosition) {	// If position > WinPosition, moves are undone.
+					position -= dieRoll;
+				}
+				if (position == WinPosition) {	// only if position = 100, win is declared.
 					System.out.println("Player moves " + dieRoll + ". current position " + position);
 					System.out.println("Player Wins!");
 					break;
 				}
 				System.out.println("Player moves " + dieRoll + ". current position " + position);
-			} else if (choice == snake) {
+			} else if (choice == snake) {	// Encountered a snake, moves back positions.
 				position -= dieRoll;
 				System.out.println("Player encounters a snake. moves back " + dieRoll + " positions."
 						+ " current position " + position);
@@ -45,7 +51,7 @@ public class SnakeAndLadderUC4 {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game simulation.");
-		SnakeAndLadderUC4 Player1 = new SnakeAndLadderUC4();
+		SnakeAndLadderUC5 Player1 = new SnakeAndLadderUC5();
 		Player1.PlayerPosition(0);
 	}
 
