031ce4665f1a03ad3e90efc4567079a256c38d53
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-14 18:40:52 +0530

    Added Welcome statement

---
+++ SnakeAndLadder.java
@@ -0,0 +1,14 @@
+/**
+ * Snake and Ladder Program.
+ * Snake and Ladder game played with single player at start position 0
+ *
+ * @author Pratik Chaudhari
+ * @since 14/06/2021
+ */
+package day4;
+
+public class SnakeAndLadderUC1 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to snake and ladder game simulation.");
+	}
+}
